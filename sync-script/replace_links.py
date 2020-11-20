3130475c1c0fd00a0f59bd1c578f3f5dc33014b3
geduardo <48300381+geduardo@users.noreply.github.com>
2020-11-20 19:21:44 +0100

    Update replace_links.py

--- sync-script/replace_links.py
+++ sync-script/replace_links.py
@@ -8,23 +8,36 @@
 
 """
 Replaces GitHub links with uid cross references for the files specified 
-in the map.csv file. This script is used to create a usable copy of 
-the files to be used in the documentation repository.
+in the uid-map.csv file. This script is used to create a usable copy of 
+the files for the documentation repository.
 
 """
-
+import click 
 import csv
-def main():
-    with open('./sync-script/map.csv') as f:
-        reader = csv.reader(f, skipinitialspace=True)
-        map_urls = dict(reader)
-    for path in map_urls:
-        with open("./Language/"+path, "rt", encoding='utf-8') as f:
+from urllib.request import urlopen
+
+@click.command()
+@click.argument("URL_MAP")
+@click.argument("SOURCE_DIR")
+def main(url_map : str, source_dir: str):
+    map_link_to_uid = extract_csv(url_map)
+    links_to_xrefs(source_dir, map_link_to_uid)
+
+def extract_csv(url_map) -> dict:
+    f = urlopen(url_map)
+    lines = [l.decode('utf-8') for l in f.readlines()]
+    reader = csv.reader(lines)
+    csv_file = dict(reader)
+    return csv_file
+
+def links_to_xrefs(source_dir, map_link_to_uid):
+    for path in map_link_to_uid:
+        with open(source_dir + '/' +path, "rt", encoding='utf-8') as f:
             text = f.readlines()
             new_text = []
             for line in text:
-                for url in map_urls:
-                    uid = map_urls[url]
+                for url in map_link_to_uid:
+                    uid = map_link_to_uid[url]
                     full_url = 'https://github.com/microsoft/qsharp-language/blob/main/Specifications/Language/'+url
                     full_uid = 'xref:'+ uid
                     line = line.replace(full_url, full_uid)
